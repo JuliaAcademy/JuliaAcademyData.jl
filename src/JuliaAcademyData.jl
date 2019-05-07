b50f32eaa68434866f2c8403e7a15d27b1654335
Matt Bauman <mbauman@gmail.com>
2019-05-07 15:02:08 -0500

    Files generated by PkgTemplates

---
+++ src/JuliaAcademyData.jl
@@ -0,0 +1,5 @@
+module JuliaAcademyData
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using JuliaAcademyData
+using Test
+
+@testset "JuliaAcademyData.jl" begin
+    # Write your own tests here.
+end
